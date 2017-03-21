66636edbe0919a84da1d15904ca47bb4768a46f5
jianlin.zjl <jianlin.zjl@alibaba-inc.com>
2017-03-21 09:58:23 +0800

    first commit

---
+++ demo/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import Demo from '../src/index';
+ReactDOM.render(<Demo />, document.getElementById('container'));
+
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React,{Component} from 'react';
+import "./index.scss";
+class ReactJsonFrom extends Component {
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+
+        };
+    }
+    render() {
+        return (
+        <div className="reactjsonfrom-component">
+            hello world
+        </div>);
+    }
+    componentDidMount(){}
+}
+
+export default ReactJsonFrom;
+
